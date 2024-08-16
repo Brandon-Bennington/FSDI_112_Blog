39a7107e022e7cb160319683cf57d793c6c2a2af
Brandon Bennington <brandon.bennington@sdgku.edu>
2024-08-16 00:08:36 -0300

    End of class 113-1

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from pages.views import HomePageView, AboutPageView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),  # Home page URL
@@ -9,4 +10,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),  # Django's built-in auth URLs
     path('accounts/', include('accounts.urls')),  # Custom account URLs
     path('posts/', include('posts.urls')),  # Include the posts app URLs
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(success_url='/accounts/password_change/done/'), name='password_change'),
+    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     UserPassesTestMixin
 )
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from .models import Post
 
@@ -24,14 +25,15 @@ class PostDetailView(DetailView):
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = "posts/new.html"
-    fields = ['title', 'subtitle', 'author', 'body', 'status']
+    fields = ['title', 'subtitle', 'body', 'status']  # Removed 'author' field
     success_url = reverse_lazy('post_list')
 
     def form_valid(self, form):
-        form.instance.author = self.request.user
+        form.instance.author = self.request.user  # Ensures the logged-in user is set as the author
         return super().form_valid(form)
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = "posts/edit.html"
     fields = ['title', 'subtitle', 'status', 'body']
@@ -41,7 +43,8 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
         post = self.get_object()
         return post.author == self.request.user
 
-class PostDeleteView(LoginRequiredMixin, DeleteView):
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = "posts/delete.html"
     success_url = reverse_lazy('post_list')
